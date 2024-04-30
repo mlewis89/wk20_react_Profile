6ba2d4383da03848ae6829f59d7da792d1e89797
Mark Lewis <mlewis89@gmail.com>
2024-04-30 18:22:59 +1000

    eslint

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ export default function Footer() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <>
-                <ul class="flex-container">
-                <a href="tel:123456789" target="_blank">1234 456 789</a>
-                <a href="mailto:mark@website.com.au" target="_blank">mark@website.com.au</a>
-                <a href="https://github.com/mlewis89/" target="_blank">Git Hub</a>
-                <a href="https://www.linkedin.com/in/mark-lewis-1948b233/" target="_blank">LinkedIn</a>
+                <ul className="flex-container">
+                <a href="tel:123456789" target="_blank" rel="noreferrer">1234 456 789</a>
+                <a href="mailto:mark@website.com.au" target="_blank" rel="noreferrer">mark@website.com.au</a>
+                <a href="https://github.com/mlewis89/" target="_blank" rel="noreferrer">Git Hub</a>
+                <a href="https://www.linkedin.com/in/mark-lewis-1948b233/" target="_blank" rel="noreferrer">LinkedIn</a>
             </ul>
     </>
   );
