a2398301257a1174a405a8bfd0bfb841d630a77b
Mark Lewis <mlewis89@gmail.com>
2024-04-30 17:48:28 +1000

    base files

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,8 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function AboutMe() {
+  return (
+    <div className="container pt-4 about-me">
+
+    </div>
+  );
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function Contact() {
+  return (
+    <div className="container pt-4 contact">
+
+    </div>
+  );
+}
