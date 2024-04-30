8dafa44df240cfe5acc3474a40c022d56c99be0f
Mark Lewis <mlewis89@gmail.com>
2024-04-30 18:20:44 +1000

    added base content from wk2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ export default function Footer() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <>
-    /footer/
+                <ul class="flex-container">
+                <a href="tel:123456789" target="_blank">1234 456 789</a>
+                <a href="mailto:mark@website.com.au" target="_blank">mark@website.com.au</a>
+                <a href="https://github.com/mlewis89/" target="_blank">Git Hub</a>
+                <a href="https://www.linkedin.com/in/mark-lewis-1948b233/" target="_blank">LinkedIn</a>
+            </ul>
     </>
   );
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,7 +2,16 @@
 export default function AboutMe() {
   return (
     <div className="container pt-4 about-me">
-/about-me/
+      <h2>About Me</h2>
+
+      <p>
+        I have an avid passion for systems that make everyday tasks easier.
+        Starting my professional career as a robotics and mechatronic engineer
+        has given me a wide base of problem solving skills. I have a passion for
+        the outdoors and adventure and this has connectted with scouting of
+        which i am privelleged to lead and work alond side many amazing people
+        from different walks of life.
+      </p>
     </div>
   );
 }
