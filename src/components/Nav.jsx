b3f411a0ef665e15634ccdf1fb68fe6c1c8d216a
Mark Lewis <mlewis89@gmail.com>
2024-04-30 17:58:15 +1000

    Basic Nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,8 +10,17 @@ export default function Nav() {
         <Link key={1} className="nav-link text-light" to="/">
           Home
         </Link>,
-        <Link key={2} className="nav-link text-light" to="/about">
-          About Us
+        <Link key={2} className="nav-link text-light" to="/about-me">
+          About Me
+        </Link>,
+        <Link key={3} className="nav-link text-light" to="/portfolio">
+          Portfolio
+        </Link>,
+        <Link key={4} className="nav-link text-light" to="/contact">
+          Contact
+        </Link>,
+        <Link key={5} className="nav-link text-light" to="/resume">
+          Resume
         </Link>,
       ]}
     />
