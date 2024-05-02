7912b718ff6689fb4b339db54624a27cb6eae491
Mark Lewis <mlewis89@gmail.com>
2024-05-02 13:29:44 +1000

    footer images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
+import gitHubLogo from '../assets/GitHub_Logo.png';
+import linkedInLogo from '../assets/LI-Logo.png';
+import xLogo from '../assets/x-logo-black.png';
 
 export default function Footer() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <>
                 <ul className="flex-container">
-                <a href="tel:123456789" target="_blank" rel="noreferrer">1234 456 789</a>
-                <a href="mailto:mark@website.com.au" target="_blank" rel="noreferrer">mark@website.com.au</a>
-                <a href="https://github.com/mlewis89/" target="_blank" rel="noreferrer">Git Hub</a>
-                <a href="https://www.linkedin.com/in/mark-lewis-1948b233/" target="_blank" rel="noreferrer">LinkedIn</a>
+                
+                <a href="https://github.com/mlewis89/" target="_blank" rel="noreferrer"><img className="footer-image" src={gitHubLogo} height={25} /></a>
+                <a href="https://www.linkedin.com/in/mark-lewis-1948b233/" target="_blank" rel="noreferrer"><img className="footer-image" src={linkedInLogo} alt="#" height={25} /></a>
+                <a href="https://twitter.com/Markdlewis89" target="_blank" rel="noreferrer"><img className="footer-image" src={xLogo} alt="#" height={25} />@marklewis89</a>
             </ul>
     </>
   );
