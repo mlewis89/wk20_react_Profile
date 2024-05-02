c13a08e37a26fde342881fc4d4ee1b5e9a6d1c59
Mark Lewis <mlewis89@gmail.com>
2024-05-02 18:32:24 +1000

    contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // Here we import a helper function that will check if the email is valid
-import { validateEmail } from '../utils/helpers';
+import { validateEmail } from "../utils/helpers";
 
 function Contact() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
-  
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
@@ -19,13 +19,14 @@ function Contact() {
     const inputValue = target.value;
 
     // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
+    if (inputType === "email") {
       setEmail(inputValue);
-    } else if (inputType === 'name') {
+    } else if (inputType === "name") {
       setName(inputValue);
     } else {
       setMessage(inputValue);
     }
+    setErrorMessage('');
   };
 
   const handleFormSubmit = (e) => {
@@ -33,48 +34,61 @@ function Contact() {
     e.preventDefault();
 
     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) ) {
-      setErrorMessage('Email is invalid');
+    if (!validateEmail(email)) {
+      setErrorMessage("Email is invalid");
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
       // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
-    
+
+    if(!name|| !message)
+    { setErrorMessage('incomplete form')
+      return;}
+
     alert(`Thankyou for your message ${name}, I'll respond shortly.`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setName('');
-    setMessage('');
-    setEmail('');
+    setName("");
+    setMessage("");
+    setEmail("");
+  };
+
+  const handleCurserMove = (e) => {
+
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "name") {
+      if(!inputValue) setErrorMessage("Name can't be Blank");
+    } else if (inputType === "email") {
+      if (!inputValue) setErrorMessage("Email can't be Blank");
+    } else {
+      if (!inputValue) setErrorMessage("Message can't be Blank");
+    }
+
+
+
   };
 
   return (
-    <div className="container">
-      <h1>Contact Me</h1>
+<div className="container">
+      <h2>Contact Me</h2>
       <form className="form" onSubmit={handleFormSubmit}>
-      <input
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="name"
-        />
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
-        
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Message"
-        />
-        <button type="submit">Submit</button>
+        <div className="form-group">
+          <input className="form-control" value={name} name="name" onChange={handleInputChange} onBlur={handleCurserMove} type="text" placeholder="name" />
+        </div>
+        <div className="form-group">
+          <input className="form-control" value={email} name="email" onChange={handleInputChange} onBlur={handleCurserMove} type="email" placeholder="email" />
+        </div>
+        <div className="form-group">
+          <textarea className="form-control" value={message} name="message" onChange={handleInputChange} onBlur={handleCurserMove} type="text" placeholder="Message" rows="10"></textarea>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
       </form>
       {errorMessage && (
         <div>
@@ -82,6 +96,8 @@ function Contact() {
         </div>
       )}
     </div>
+    
+
   );
 }
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,34 @@
 // This is a static page mocking an "About Us" section for our fake user data
+
 export default function Resume() {
   return (
-    <div className="container pt-4 resume">
-/resume/
+    <div className="container resume">
+<div className="row">
+<div className="col w-50">
+      <h2>Proficiencies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>Web APIs</li>
+        <li>Third Party APIs</li>
+        <li>ServerSide APIs</li>
+        <li>NodeJs</li>
+        <li>Object oriented Proraming (OOP)</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>Model View Controller (MVC)</li>
+        <li>Regular expressions</li>
+        <li>NoSql - Mongo db</li>
+        <li>Progressive web app (PWA)</li>
+        <li>React</li>
+        <li>MERN stack</li>
+      </ul>
+      </div>
+      <div className="col w-50 align-middle">
+      <h4> Click <a href='https://www.linkedin.com/in/mark-lewis-1948b233/'>here</a> to download my resume</h4>
+    </div>
+    </div>
     </div>
   );
 }
